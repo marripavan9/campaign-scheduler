088f8be8845f86307bfbcf99252dbac9e0e39146
pavan <pavan.marri@zemosolabs.com>
2023-12-29 10:34:26 +0530

    added parallel processing for rerun job.

--- src/main/java/com/zemoso/scheduler/job/ReRunEmailCampaignJob.java
+++ src/main/java/com/zemoso/scheduler/job/ReRunEmailCampaignJob.java
@@ -9,14 +9,24 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 public class ReRunEmailCampaignJob implements Job {
 
+    private static final int THREAD_POOL_SIZE = 10;
 
     @Override
     public void execute(JobExecutionContext jobExecutionContext) {
         Date startDate = new Date();
+        ExecutorService executorService = Executors.newFixedThreadPool(THREAD_POOL_SIZE);
+        List<Future<Void>> futures = new ArrayList<>();
+
         try (Connection conn = DatabaseOperation.getConnection()) {
             String selectQuery = "SELECT c.*, cr.id AS run_id FROM campaign c " +
                     "INNER JOIN campaign_run cr ON c.id = cr.campaign_id " +
@@ -27,17 +37,29 @@ public class ReRunEmailCampaignJob implements Job {
                     while (resultSet.next()) {
                         int campaignId = resultSet.getInt("id");
                         int runId = resultSet.getInt("run_id");
-                        EmailTriggerJobService.triggerEmailsAndUpdateStatus(conn, campaignId, runId);
+                        Future<Void> future = executorService.submit(() -> {
+                            EmailTriggerJobService.triggerEmailsAndUpdateStatus(conn, campaignId, runId);
+                            return null;
+                        });
+                        futures.add(future);
                     }
                 }
             }
-            Date endDate = new Date();
-            long executionTime = endDate.getTime() - startDate.getTime();
-            double executionTimeInSeconds = executionTime / 1000.0;
-            System.out.println("Execution time: " + executionTimeInSeconds + " seconds");
-        } catch (SQLException e) {
+
+            for (Future<Void> future : futures) {
+                future.get();
+            }
+
+        } catch (SQLException | InterruptedException | ExecutionException e) {
             e.printStackTrace();
+        } finally {
+            executorService.shutdown();
         }
+
+        Date endDate = new Date();
+        long executionTime = endDate.getTime() - startDate.getTime();
+        double executionTimeInSeconds = executionTime / 1000.0;
+        System.out.println("Execution time: " + executionTimeInSeconds + " seconds");
         System.out.println("Processing Done");
     }
 
